f51bca76c753f69231384476c4d685b44890a9c1
Brian <riceb53@gmail.com>
2019-08-15 19:24:03 -0500

    nest note-selectors inside of note-container

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,19 @@
 <template>
- <div id="app">
-  <toolbar />
-   <div class="note-container">
-     <div class="note-selectors">
-       <div class="note-selector active">
-         <p class="note-selector-title">First note...</p>
-         <p class="note-selector-timestamp">Timestamp here...</p>
-       </div>
-       <div class="note-selector">
-         <p class="note-selector-title">Second note...</p>
-         <p class="note-selector-timestamp">Timestamp here...</p>
-       </div>
-       <div class="note-selector">
-         <p class="note-selector-title">Third note...</p>
-         <p class="note-selector-timestamp">Timestamp here...</p>
-       </div>
-     </div>
-     <div class="note-editor">
-       <p class="note-editor-info">Timestamp here...</p>
-       <textarea class="note-editor-input">
-         First note...
-
-         Note text here...
-       </textarea>
-     </div>
-   </div>
- </div>
+  <div id="app">
+    <toolbar />
+    <note-container />
+  </div>
 </template>
 
 <script>
   import Toolbar from "./components/Toolbar.vue"
+  import NoteContainer from "./components/NoteContainer.vue"
 
   export default {
    name: 'app',
    components: {
-    Toolbar
+    Toolbar,
+    NoteContainer
    }
   };
 </script>
---
+++ src/components/NoteContainer.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="note-container">
+    <note-selectors />
+    <div class="note-editor">
+      <p class="note-editor-info">Timestamp here...</p>
+      <textarea class="note-editor-input">
+        First note...
+
+        Note text here...
+      </textarea>
+    </div>
+  </div>
+</template>
+
+<script>
+  import NoteSelectors from "./NoteSelectors.vue"
+  export default {
+    name: 'note-container',
+    components: {
+      NoteSelectors
+    }
+  }
+</script>
