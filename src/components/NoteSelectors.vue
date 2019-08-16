922d448674a98a0a298b9405bb84b72ad75c3688
Brian <riceb53@gmail.com>
2019-08-15 20:02:07 -0500

    loop through notes to render note-selector

--- src/components/NoteSelectors.vue
+++ src/components/NoteSelectors.vue
@@ -1,16 +1,26 @@
 <template>
   <div class="note-selectors">
-    <note-selector />
+    <note-selector v-for="note in notes"/>
   </div>
 </template>
 
 <script>
-  import NoteSelector from "./NoteSelector.vue"
+import NoteSelector from "./NoteSelector.vue"
 
 export default {
   name: 'note-selectors',
   components: {
     NoteSelector
+  },
+  data: function() {
+    return {
+      notes: [
+        { id: 1, body: "This is a first test", timestamp: Date.now() },
+        { id: 2, body: "This is a second test", timestamp: Date.now() },
+        { id: 3, body: "This is a third test", timestamp: Date.now() },
+        { id: 4, body: "This is a fourth test", timestamp: Date.now() }
+      ]
+    }
   }
 }
 
