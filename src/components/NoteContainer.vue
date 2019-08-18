063244132bec1673bbca46234d06d74ee94f8d20
Brian <riceb53@gmail.com>
2019-08-18 12:43:21 -0500

    make some app

--- src/components/NoteContainer.vue
+++ src/components/NoteContainer.vue
@@ -2,18 +2,21 @@
   <div class="note-container">
     <note-selectors v-bind:notes="notes"/>
     <note-editor />
+    <multi-select />
   </div>
 </template>
 
 <script>
   import NoteSelectors from "./NoteSelectors.vue"
   import NoteEditor from "./NoteEditor.vue"
+  import MultiSelect from "./MultiSelect.vue"
 
   export default {
     name: 'note-container',
     components: {
       NoteSelectors,
-      NoteEditor
+      NoteEditor,
+      MultiSelect
     },
     props: ['notes']
   }
